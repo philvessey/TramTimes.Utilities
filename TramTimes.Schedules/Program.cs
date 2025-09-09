0c4769b09823ce3c6528b1ffe1f2adb0a456767c
Phil Vessey <philvessey@icloud.com>
2025-09-09 15:30:21 +0100

    Create folders

--- TramTimes.Schedules/Program.cs
+++ TramTimes.Schedules/Program.cs
@@ -1,7 +1,37 @@
 ﻿using Spectre.Console;
 
+#region write banner
+
 AnsiConsole.Write(renderable: new FigletText(text: "Schedules")
     .Color(color: Color.Blue)
     .LeftJustified());
 
-AnsiConsole.MarkupLine(value: "[grey]Creates structured JSON files with weekly schedules[/]");
+#endregion
+
+#region write description
+
+AnsiConsole.MarkupLine(value: "[grey]Creates structured JSON files with weekly schedules for tram stops[/]");
+AnsiConsole.WriteLine();
+
+#endregion
+
+#region create folders
+
+Directory.CreateDirectory(path: "input");
+Directory.CreateDirectory(path: "output");
+
+#endregion
+
+#region check folders
+
+var file = Path.Combine(
+    path1: "input",
+    path2: "stops.txt");
+
+if (!File.Exists(path: file))
+{
+    AnsiConsole.MarkupLine(value: $"[red]❌ Error: {file} not found[/]");
+    AnsiConsole.MarkupLine(value: $"[yellow]Please create a text file with stop IDs at {file}, one per line[/]");
+}
+
+#endregion
