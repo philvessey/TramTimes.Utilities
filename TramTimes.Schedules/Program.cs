d2f29e575b811b03c701cf9587c71ff3c01a3f43
Phil Vessey <philvessey@icloud.com>
2025-09-09 14:36:19 +0100

    Initial commit

---
+++ TramTimes.Schedules/Program.cs
@@ -0,0 +1,7 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Write(renderable: new FigletText(text: "Schedules")
+    .Color(color: Color.Blue)
+    .LeftJustified());
+
+AnsiConsole.MarkupLine(value: "[grey]Creates structured JSON files with weekly schedules[/]");
