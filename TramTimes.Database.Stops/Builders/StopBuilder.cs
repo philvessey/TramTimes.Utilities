346d7fda513a7d4989a83f16f752ba997614dbea
Phil Vessey <philvessey@icloud.com>
2025-09-29 13:27:16 +0100

    Add database process

---
+++ TramTimes.Database.Stops/Builders/StopBuilder.cs
@@ -0,0 +1,24 @@
+namespace TramTimes.Database.Stops.Builders;
+
+public static class StopBuilder
+{
+    public static async Task<string[]> BuildAsync(string path)
+    {
+        #region build stops
+        
+        var stops = await File.ReadAllLinesAsync(path: path);
+        
+        #endregion
+        
+        #region build results
+        
+        var results = stops
+            .Where(predicate: id => !string.IsNullOrWhiteSpace(value: id))
+            .Select(selector: id => id.Trim())
+            .ToArray();
+        
+        #endregion
+        
+        return results;
+    }
+}
